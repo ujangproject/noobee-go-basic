eabc1b2bc2f24fd0bbffaad9e74d2f48716b61e0
muhammadtiolaksono-alpha <124859731+muhammadtiolaksono-alpha@users.noreply.github.com>
2023-10-05 21:00:08 +0700

    up constant

---
+++ constant/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	const DATABASE = "127.0.0.1"
+	const USERNAME = "root"
+	const PASSWORD = "root"
+
+	fmt.Println("Database Name", DATABASE)
+	fmt.Println("Database User", USERNAME)
+	fmt.Println("Database Password", PASSWORD)
+
+}
