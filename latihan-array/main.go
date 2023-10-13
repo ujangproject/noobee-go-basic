b6f0f55239c57a4dc6b6bf51ebdd86801f7de4d4
muhammadtiolaksono-alpha <124859731+muhammadtiolaksono-alpha@users.noreply.github.com>
2023-10-13 09:58:05 +0700

    Latihan Array

---
+++ latihan-array/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var buah [3]string
+	buah[0] = "Apel"
+	buah[1] = "Pisang"
+	buah[2] = "Mangga"
+
+	fmt.Println("Nama Buah Pertama", buah[0])
+	fmt.Println("Nama Buah Kedua", buah[1])
+	fmt.Println("Nama Buah Ketiga", buah[2])
+}
